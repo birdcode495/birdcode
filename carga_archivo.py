ebe441d0d4fdf2e2262470e376cdc589d99fb196
BirdCode <birdcode.py@gmail.com>
2026-07-26 17:50:49 -0500

    Ajustes parte 7

--- carga_archivo.py
+++ carga_archivo.py
@@ -12,7 +12,8 @@ with col_init2:
 
     st.image('Images/Eagle4.png', width = 100)
 
-uploaded_files = st.file_uploader("Cargar ficheros", accept_multiple_files="directory", type=['geojson', 'kml', 'dbf', 'cpg', 'prj', 'shp', 'shx']
+uploaded_files = st.file_uploader("Cargar ficheros", accept_multiple_files="directory", 
+    type=['geojson', 'kml', 'dbf', 'cpg', 'prj', 'shp', 'shx', 'gpkg']
 )
 # for uploaded_file in uploaded_files:
 #     st.image(uploaded_file)
