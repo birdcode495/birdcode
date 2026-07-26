34e1110a09e6f77bff9acc083c19ae94eb15752c
BirdCode <birdcode.py@gmail.com>
2026-07-26 11:21:48 -0500

    Construccion card de pagina inicio de sesion

--- cards.py
+++ cards.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 def cargar_archivo_link():
 
-	st.page_link('carga_archivo.py', label = 'Cargar archivo', icon = ':material/upload_file:')
-	
+	st.page_link('login.py', label = 'Inicia sesión o registrate', icon = ':material/login:')
+	st.image('Images/Login.png', width = 290, use_container_width = True)
