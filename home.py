7723c8bdbe73944c0a6d1f373cd5847ddb59afda
BirdCode <birdcode.py@gmail.com>
2026-07-26 11:33:33 -0500

    Ajustes parte 2

--- home.py
+++ home.py
@@ -1,6 +1,21 @@
 import streamlit as st
 from cards import cargar_archivo_link
 
+
+col_init1, col_init2 = st.columns([2,1])
+
+
+with col_init1:
+
+	welcome = st.empty()
+	welcome.header('Explorador de BirdCode App')
+
+
+with col_init2:
+
+	st.image('Images/Eagle.png', width = 70)
+
+
 cols = st.columns(2)
 
 with cols[0].container(height = 310):
